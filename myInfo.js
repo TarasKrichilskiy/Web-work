1b2b3fa99484e0786e0d0edfe266b6cda581bb34
TarasKrichilskiy <94582910+TarasKrichilskiy@users.noreply.github.com>
2024-08-30 15:03:16 -0700

    Update myInfo.js

--- myInfo.js
+++ myInfo.js
@@ -1,6 +1,6 @@
 
 function updateLinkedInInfoText() {
-    let linkedInInfoElement = document.getElementById('LinkedInInfo');
+    let linkedInInfoElement = document.getElementById('result1');
 
     linkedInInfoElement.textContent = `Simplicity is a quality that, once lost, 
                                        can be extraordinarily difficult to recapture`;
